08d4665d79fabf64d6a14fa9d85e641d37781da9
Tim Kunold <kunold@lx201.tcs.uni-luebeck.de>
2010-02-17 11:48:52 +0100

    implemented localWordlistComputation

--- code/yaquix/src/main/java/yaquix/phase/attribute/wordlist/LocalWordlistComputation.java
+++ code/yaquix/src/main/java/yaquix/phase/attribute/wordlist/LocalWordlistComputation.java
@@ -1,6 +1,11 @@
 package yaquix.phase.attribute.wordlist;
 
 import java.util.List;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.Vector;
+import java.util.logging.Logger;
 
 import yaquix.Connection;
 import yaquix.knowledge.Mails;
@@ -34,27 +39,63 @@ class LocalWordlistComputation extends SymmetricPhase {
 	 */
 	private OutputKnowledge<List<String>> localWordlist;
 	
+	private Logger logger;
+	
 	/**
 	 * This creates a new local discretization phase
 	 * @param localMails the mails to compute a word list for
-	 * @param ownWordlist the selected word list
+	 * @param localWordlist the selected word list
 	 */
 	public LocalWordlistComputation(InputKnowledge<Mails> localMails,
 			OutputKnowledge<List<String>> localWordlist) {
+		logger.info("localWordlistcomputation");
+		
 		this.localMails = localMails;
 		this.localWordlist = localWordlist;
 	}
 
 	@Override
 	protected void execute(Connection connection) {
-		// TODO execute
-
+		logger.info("localWordlistcomputation: starting computation...");
+		
+		Mails mails = localMails.get();
+		List<String> words = new Vector<String>();
+		List<Double> spamPercentage = new Vector<Double>();
+		List<Double> nonSpamPercentage = new Vector<Double>();
+		
+		logger.info("localWordlistComputation: computing words in our emails and corresponding spam-/nonSpam-percentages...");
+		
+		Set<String> wordset = mails.getAllWords();
+		
+		for(String word : wordset){
+			words.add(word);
+			spamPercentage.add((double) mails.countWordInSpamMails(word)*100/mails.countWordInAllEmails(word));
+			nonSpamPercentage.add((double) mails.countWordInNonSpamMails(word)*100/mails.countWordInAllEmails(word));
+		}
+		
+		localWordlist.put(chooseWords(words, spamPercentage,  nonSpamPercentage));
 	}
 	
 	private List<String> chooseWords(List<String> words, 
 									 List<Double> spamPercentage, 
 									 List<Double> nonSpamPercentage) {
-		//TODO: chooseWords
-		return null;
+		
+		logger.info("localWordlistComputation: choosing the best  words (max: " + NUMBER_OF_WORDS  + "...");
+		
+		double key;
+		List<String> goodWords = new Vector<String>();
+		SortedMap<Double, String> map = new TreeMap<Double, String>();
+		
+		for(int i = 0; i < words.size(); i++){			
+			map.put((Math.abs(spamPercentage.get(i) - nonSpamPercentage.get(i))), words.get(i));
+		}
+		
+		for(int i = 0; i < NUMBER_OF_WORDS; i++){
+			key = map.lastKey();
+			goodWords.add(map.get(key));
+			map.remove(key);
+		}
+		
+		return goodWords;
 	}
 }
