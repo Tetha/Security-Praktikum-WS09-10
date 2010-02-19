d4169391e9881d8b57912a981548a41881ce5f61
Harald Kraemer <harryk@freenet.de>
2010-02-19 16:41:45 +0100

    new convenience phase

---
+++ code/yaquix/src/main/java/yaquix/phase/ExchangeMailCount.java
@@ -0,0 +1,34 @@
+package yaquix.phase;
+
+import java.io.IOException;
+
+import yaquix.Connection;
+
+public class ExchangeMailCount extends SymmetricPhase {
+
+	
+	/**
+	 * contains the local number of mails
+	 */
+	InputKnowledge<Integer> localMailCount;
+
+	/**
+	 * contains the remote number of mails
+	 */
+	OutputKnowledge<Integer> remoteMailCount;
+
+	public ExchangeMailCount(InputKnowledge<Integer> localMailCount,
+			OutputKnowledge<Integer> remoteMailCount) {
+		super();
+		this.remoteMailCount = remoteMailCount;
+		this.localMailCount = localMailCount;
+	}
+
+	@Override
+	protected void execute(Connection connection) throws IOException,
+			ClassNotFoundException {
+		remoteMailCount.put(connection.exchangeInteger(localMailCount.get()));
+
+	}
+
+}
