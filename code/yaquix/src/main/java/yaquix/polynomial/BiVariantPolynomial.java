33f489b165edfe8d0980506f9fec322b41e4101d
Tim Kunold <tim_kunold@web.de>
2010-02-22 11:39:12 +0100

    implemented bivariantPolynomial

--- code/yaquix/src/main/java/yaquix/polynomial/BiVariantPolynomial.java
+++ code/yaquix/src/main/java/yaquix/polynomial/BiVariantPolynomial.java
@@ -10,23 +10,28 @@ public class BiVariantPolynomial {
 	 * stores the coefficients, such that it is
 	 * coefficients[i][j] * x^i * y^j.
 	 */
-	int[][] coefficients; 
-	
+	int[][] coefficients;
+
 	/**
-	 * This constructs a polynomial in two variables. 
-	 * 
-	 * The coefficient in the array must be arranged such that the 
+	 * This constructs a polynomial in two variables.
+	 *
+	 * The coefficient in the array must be arranged such that the
 	 * polynomial is represented by coefficients[i][j] * x^i * y^j.
-	 * 
-	 * The array is copied in the constructor in order to avoid potential 
+	 *
+	 * The array is copied in the constructor in order to avoid potential
 	 * problems if the coefficients array is modified after passing it
 	 * into a polynomial.
 	 * @param coefficients
 	 */
 	public BiVariantPolynomial(int[][] coefficients) {
-		// TODO: constructor
+
+		this.coefficients = new int[coefficients.length][coefficients[0].length];
+
+		for(int i = 0; i < coefficients.length; i++){
+			this.coefficients[i] = coefficients[i].clone();
+		}
 	}
-	
+
 	/**
 	 * evaluates the polynomial in the given point
 	 * @param x the value of the first variable
@@ -34,7 +39,15 @@ public class BiVariantPolynomial {
 	 * @return the value of the polynomial in the given coordinate
 	 */
 	public int evaluate(int x, int y) {
-		// TODO evaluate
-		return -1;
+
+		int result = 0;
+
+		for(int i = 0; i<2; i++){
+			for(int j =0; j<3; j++){
+				result += coefficients[i][j] * Math.pow(x, i) * Math.pow(y, j);
+			}
+		}
+
+		return result;
 	}
 }
